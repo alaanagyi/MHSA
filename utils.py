c0ca3faa64597ab4599cd5234cb74d4f1e676e77
alaa nagy <83980367+alaanagyi@users.noreply.github.com>
2024-04-28 04:18:09 +0300

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,214 +1,234 @@
 import torch
-from torch import optim, nn
-
-import torch.distributed as dist
 from torch import Tensor
+from torchvision import transforms
+import torchvision.transforms.functional as TF
 
-import numpy as np
-import random
-import math
+from einops import rearrange
+from copy import copy, deepcopy
+
 import os
+from PIL import Image
+from typing import Sequence, Dict, Union, List, Tuple, Optional
 
-import models
-import moco
-import losses
-
-
-def init_seeds(seed: int) -> None:
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    random.seed(seed)
-    np.random.seed(seed)
+from .spatial_transforms import Pepper, Salt, RandomCrop, RandomGaussianBlur
+from .temporal_transforms import RandomReverse, UniformDownsample
 
 
-def get_models(args):
-    if len(args.sources) == 1:
-        encoder = models.SMSV(
-            sources=args.sources,
-            backbone="r3d_18",
-            pretrained=True,
-            return_features=False,
-        )
-    else:
-        backbones = {}
-        for source in args.sources:
-            backbone = models.SMSV(
-                sources=[source],
-                backbone="r3d_18",
-                pretrained=True,
-                return_features=True,
-            )
-            ckpt_name = "best_dtc_pr.pth" if args.task == "detection" else "best_cls_pr.pth"
-            backbone_ckpt_path = os.path.join(args.ckpt_dir_path, "single", f"SMSV_{args.classifier}_{source}_{args.task}", ckpt_name)
-            backbone_ckpt = torch.load(backbone_ckpt_path, map_location="cpu")
-            backbone.load_state_dict(backbone_ckpt["encoder"], strict=True)
-            backbones[source] = backbone
+class LabelTransform(object):
+    """
+    Transform the labels of DAD from str to torch.Tensor.
+    """
+    def __init__(self, task: str) -> None:
+        super().__init__()
+        assert task in ["detection", "classification"]
+        self.task = task
+        self.dictionary = {
+            "normal_driving": 0,
+            "adjusting_radio": 1,
+            "drinking": 2,
+            "messaging_left": 3,
+            "messaging_right": 4,
+            "reaching_behind": 5,
+            "talking_with_passenger": 6,
+            "talking_with_phone_left": 7,
+            "talking_with_phone_right": 8,
+            "unknown": 9
+        }
 
-        encoder = models.MMMV(
-            backbones=backbones,
-            fusion_method=args.fusion_method,
-            fusion_steps=args.fusion_steps,
-            mask_ratio=args.mask_ratio,
-            backbone_out_channels=512,
-            dropout=args.dropout,
-            freeze_backbone=args.freeze_backbone
-        )
-
-    in_dim, out_dim = encoder.out_dim, args.contrast_dim
-    hidden_dim = min(max(in_dim // 2, out_dim * 2), in_dim)
-    projector = models.MLP(in_dim=in_dim, hidden_dim=hidden_dim, out_dim=out_dim, normalize=True)
-    model = moco.SuMoCo(base_encoder=encoder, mlp=projector, dim=out_dim, K=args.K, m=args.m)
-
-    num_classes = 1 if args.task == "detection" else 9
-    if args.classifier == "mlp":
-        classifier = models.MLP(in_dim=in_dim, hidden_dim=hidden_dim, out_dim=num_classes, normalize=False)
-    else:
-        classifier = models.MemoryBank(num_classes=num_classes, dim=in_dim)
-
-    return model, classifier
-
-
-def get_loss_functions(args):
-    if args.task == "detection":
-        # loss_fn_con = losses.DetConLoss(T=args.T)
-        loss_fn_con = losses.SupConLoss(T=args.T)
-        if args.classifier == "mlp":
-            loss_fn_cls = nn.BCEWithLogitsLoss(reduction="sum")
+    def __call__(self, orig_label: str) -> Tensor:
+        if self.task == "classification":
+            if "normal_driving" in orig_label:
+                return torch.tensor(self.dictionary["normal_driving"])
+            elif orig_label in self.dictionary.keys():
+                return torch.tensor(self.dictionary[orig_label])
+            else:
+                return torch.tensor(self.dictionary["unknown"])
         else:
-            loss_fn_cls = None
+            if "normal_driving" in orig_label:
+                return torch.tensor(1)
+            else:
+                return torch.tensor(0)
 
-    else:
-        loss_fn_con = losses.SupConLoss(T=args.T)
-        if args.classifier == "mlp":
-            loss_fn_cls = losses.FocalLoss()
+
+class LabelInverseTransform(object):
+    """
+    Transform the labels of DAD from str to torch.Tensor.
+    """
+    def __init__(self, task: str) -> None:
+        super().__init__()
+        assert task in ["detection", "classification"]
+        self.task = task
+        self.dictionary = {
+            0: "normal driving",
+            1: "adjusting radio",
+            2: "drinking",
+            3: "messaging (left)",
+            4: "messaging (right)",
+            5: "reaching behind",
+            6: "talking with passenger",
+            7: "talking with phone (left)",
+            8: "talking with phone (right)",
+            9: "unknown"
+        }
+
+    def __call__(self, label: Union[int, Tensor]) -> str:
+        label = label.numpy() if isinstance(label, Tensor) else label
+        label = int(label)
+
+        if self.task == "classification":
+            return self.dictionary[label]
         else:
-            loss_fn_cls = None
-
-    return loss_fn_con, loss_fn_cls
+            return "normal" if label == 1 else "anomalous"
 
 
-def get_model_optimizers(args, model):
-    optimizer_con = optim.Adam(
-        params=filter(lambda p: p.requires_grad, model.parameters()),
-        lr=args.learning_rate,
-        weight_decay=args.weight_decay
-    )
-
-    scheduler_con = optim.lr_scheduler.CosineAnnealingWarmRestarts(
-        optimizer=optimizer_con,
-        T_0=5,
-        T_mult=1,
-        eta_min=1e-6,
-        verbose=True
-    )
-
-    return optimizer_con, scheduler_con
+def load_frames(img_base_dir, source, img_names):
+    clip_source = []
+    for img_name in img_names:
+        img_path = os.path.join(img_base_dir, source, img_name)
+        with Image.open(img_path) as img:
+            img_ = deepcopy(img)
+        clip_source.append(TF.to_tensor(copy(img_.convert("L"))))
+    return clip_source
 
 
-def get_head_optimizers(args, classifier):
-    if args.classifier == "mlp":
-        optimizer_cls = optim.Adam(
-            params=classifier.parameters(),
-            lr=min(0.01, args.learning_rate * 10),
-            weight_decay=args.weight_decay
-        )
-        scheduler_cls = optim.lr_scheduler.CosineAnnealingWarmRestarts(
-            optimizer=optimizer_cls,
-            T_0=5,
-            T_mult=1,
-            eta_min=1e-5,
-            verbose=True
-        )
-    else:
-        optimizer_cls, scheduler_cls = None, None
+class TrainTransform(object):
+    def __init__(
+        self,
+        out_size: Sequence[int],
+        means: Dict[str, Union[float, Sequence[float]]],
+        stds: Dict[str, Union[float, Sequence[float]]],
+        num_augs: Optional[int] = None,
+    ) -> None:
+        super().__init__()
+        assert len(out_size) == 3
+        temporal_size, spatial_size = out_size[0], out_size[1:]
+        self.temporal_size = temporal_size
+        self.spatial_size = spatial_size
 
-    return optimizer_cls, scheduler_cls
+        means_keys, stds_keys = list(means.keys()), list(stds.keys())
+        means_keys.sort(), stds_keys.sort()
+        assert means_keys == stds_keys
+        self.data_sources = means_keys
+        self.means = means
+        self.stds = stds
 
+        self.num_augs = num_augs if num_augs is not None else 0
 
-def print_info(info: str, decorator: str = "=") -> None:
-    _, num_cols = os.popen("stty size", "r").read().split()
-    num_cols = int(num_cols)
-    assert len(decorator) == 1
-    print(decorator * num_cols)
-    if len(info) < num_cols:
-        print(f"{info:^{num_cols}s}")
-    else:
-        print(info)
-    print(decorator * num_cols)
+        self.temporal_downsample = UniformDownsample(num_frames=self.temporal_size)
+        self.spatial_downsample = transforms.Resize(size=self.spatial_size, interpolation=transforms.InterpolationMode.BILINEAR)
 
+        if self.num_augs > 0:
+            self.temporal_reverse = RandomReverse(p=0.5)
+            self.spatial_transforms = transforms.Compose([
+                transforms.RandomApply([transforms.ColorJitter(0.1, 0.1, 0.1, 0.1)], p=0.5),
+                transforms.RandomApply([transforms.RandomRotation(degrees=10)], p=0.5),
+                transforms.RandomApply([RandomGaussianBlur(kernel_sizes=[1, 3, 5], sigma=(0.1, 2.0))], p=0.5),
+                transforms.RandomApply([RandomCrop(scales=(0.75, 1.0))], p=0.5),
+            ])
+            self.add_pepper_salt = transforms.Compose([
+                transforms.RandomApply([Pepper(pepperness=5e-3)], p=0.5),
+                transforms.RandomApply([Salt(saltness=5e-3)], p=0.5)
+            ])
 
-def reduce_mean(tensor, nprocs):
-    rt = tensor.clone()
-    dist.all_reduce(rt, op=dist.ReduceOp.SUM)
-    rt /= nprocs
-    return rt
+    def __call__(
+        self,
+        img_base_dir: str,
+        sources: List[str],
+        img_names: List[str]
+    ) -> Dict[str, Tensor]:
+        img_names = self.temporal_downsample(img_names)
 
+        sources.sort()
+        assert set(sources).issubset(self.data_sources)
+        clip = {source: [] for source in sources}
 
-class SequentialDistributedSampler(torch.utils.data.sampler.Sampler):
-    """
-    Distributed Sampler that subsamples indicies sequentially, making it easier to collate all results at the end.
+        for source in sources:
+            clip_source_orig = load_frames(img_base_dir, source, img_names)
 
-    Even though we only use this sampler for eval and predict (no training), which means that the model params won't have to be synced (i.e. will not hang for synchronization even if varied number of forward passes), we still add extra samples to the sampler to make it evenly divisible (like in `DistributedSampler`) to make it easy to `gather` or `reduce` resulting tensors at the end of the loop.
+            if self.num_augs > 0:
+                for _ in range(self.num_augs):
+                    clip_source_aug = self.temporal_reverse(copy(clip_source_orig))
+                    clip_source_aug = torch.stack(clip_source_aug, dim=0)  # t, c, h, w
+                    clip_source_aug = self.spatial_transforms(clip_source_aug)
+                    clip_source_aug = self.add_pepper_salt(clip_source_aug)
+                    clip_source_aug = self.spatial_downsample(clip_source_aug)
+                    clip_source_aug = TF.normalize(clip_source_aug, mean=self.means[source], std=self.stds[source])
+                    clip_source_aug = rearrange(clip_source_aug, "t c h w -> c t h w")
 
-    Source: https://huggingface.co/transformers/v3.0.2/_modules/transformers/trainer.html
-    """
+                    clip[source].append(clip_source_aug)
 
-    def __init__(self, dataset, batch_size, rank=None, num_replicas=None):
-        if num_replicas is None:
-            if not dist.is_available():
-                raise RuntimeError("Requires distributed package to be available")
-            num_replicas = dist.get_world_size()
-        if rank is None:
-            if not dist.is_available():
-                raise RuntimeError("Requires distributed package to be available")
-            rank = dist.get_rank()
-        self.dataset = dataset
-        self.num_replicas = num_replicas
-        self.rank = rank
-        self.batch_size = batch_size
-        self.num_samples = int(math.ceil(len(self.dataset) * 1.0 / self.batch_size / self.num_replicas)) * self.batch_size
-        self.total_size = self.num_samples * self.num_replicas
+            else:
+                clip_source_orig = torch.stack(clip_source_orig, dim=0)  # t, c, h, w
+                clip_source_orig = self.spatial_downsample(clip_source_orig)  # t, c, h, w
+                clip_source_orig = TF.normalize(clip_source_orig, mean=self.means[source], std=self.stds[source])
+                clip_source_orig = rearrange(clip_source_orig, "t c h w -> c t h w")
+                clip[source].append(clip_source_orig)
 
-    def __iter__(self):
-        indices = list(range(len(self.dataset)))
-        # add extra samples to make it evenly divisible
-        indices += [indices[-1]] * (self.total_size - len(indices))
-        # subsample
-        indices = indices[self.rank * self.num_samples: (self.rank + 1) * self.num_samples]
-        return iter(indices)
+            clip[source] = torch.stack(clip[source], dim=0)  # num_augs (or 1), c, t, h, w
 
-    def __len__(self):
-        return self.num_samples
-
-
-def distributed_concat(tensor: Tensor, num_total_examples: int):
-    """
-    Concatenate all tensors across all devices.
-    Source: https://huggingface.co/transformers/v3.0.2/_modules/transformers/trainer.html
-
-    Args:
-        tensor (Tensor): the tensor to be concatenated.
-        num_total_examples (int): the total number of such tensor (i.e. the total batch size).
-
-    Returns:
-        concat (Tensor): the concatenated tensor.
-    """
-    output_tensors = [tensor.clone() for _ in range(dist.get_world_size())]
-    dist.all_gather(output_tensors, tensor)
-    concat = torch.cat(output_tensors, dim=0)
-    concat = concat[:num_total_examples]  # truncate the dummy elements added by SequentialDistributedSampler
-    return concat
+        return clip
 
 
-def concat_all_gather(tensor):
-    """
-    Performs all_gather operation on the provided tensors.
-    *** Warning ***: torch.distributed.all_gather has no gradient.
-    """
-    tensors_gather = [
-        torch.ones_like(tensor) for _ in range(dist.get_world_size())
-    ]
-    dist.all_gather(tensors_gather, tensor, async_op=False)
+class TestTransform(object):
+    def __init__(
+        self,
+        out_size: Sequence[int],
+        means: Dict[str, Union[float, Sequence[float]]],
+        stds: Dict[str, Union[float, Sequence[float]]],
+    ) -> None:
+        super().__init__()
+        assert len(out_size) == 3
+        temporal_size, spatial_size = out_size[0], out_size[1:]
+        self.temporal_size = temporal_size
+        self.spatial_size = spatial_size
 
-    output = torch.cat(tensors_gather, dim=0)
-    return output
+        means_keys, stds_keys = list(means.keys()), list(stds.keys())
+        means_keys.sort(), stds_keys.sort()
+        assert means_keys == stds_keys
+        self.data_sources = means_keys
+        self.means = means
+        self.stds = stds
+
+        self.temporal_downsample = UniformDownsample(num_frames=self.temporal_size)
+        self.spatial_downsample = transforms.Resize(size=self.spatial_size, interpolation=transforms.InterpolationMode.BILINEAR)
+
+    def __call__(
+        self,
+        img_base_dir: str,
+        sources: List[str],
+        img_names: List[str]
+    ) -> Dict[str, Tensor]:
+        img_names = self.temporal_downsample(img_names)
+
+        sources.sort()
+        assert set(sources).issubset(self.data_sources)
+        clip = {}
+
+        for source in sources:
+            clip_source_orig = load_frames(img_base_dir, source, img_names)
+            clip_source_orig = torch.stack(clip_source_orig, dim=0)  # t, c, h, w
+            clip_source_orig = self.spatial_downsample(clip_source_orig)  # t, c, h, w
+            clip_source_orig = TF.normalize(clip_source_orig, mean=self.means[source], std=self.stds[source])
+            clip_source_orig = rearrange(clip_source_orig, "t c h w -> c t h w")
+            clip[source] = clip_source_orig  # c, t, h, w
+
+        return clip
+
+
+def collate_fn(batch: List[Tuple[Dict[str, Tensor], Tensor]]) -> Tuple[List[Dict[str, Tensor]], List[Tensor]]:
+    clips = {}
+    labels = []
+    for clip, label in batch:
+        for k in clip.keys():
+            if k not in clips.keys():
+                clips[k] = [clip[k]]
+            else:
+                clips[k].append(clip[k])
+        labels.append(label)
+
+    for k in clips.keys():
+        clips[k] = torch.stack(clips[k], dim=0)  # bs, c, t, h, w
+
+    labels = torch.stack(labels, dim=0)  # bs
+
+    return clips, labels
